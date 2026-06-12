06d0562fe5ef4e1ce8313c30a3369f4e9ff602b5
transgerda <info@martijnvanboven.nl>
2026-06-12 14:09:18 +0200

    test break

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -32,7 +32,7 @@ class CalculatorTest extends TestCase
         $result = $this->calculator->add($a, $b);
 
         // Assert
-        $this->assertEquals(8, $result);
+        $this->assertEquals(7, $result);
     }
 
     /*
