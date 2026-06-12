a64f26e4ed8833a640783fe7e9a27fd783f138b3
transgerda <info@martijnvanboven.nl>
2026-06-12 14:17:39 +0200

    test lukt

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -32,7 +32,7 @@ class CalculatorTest extends TestCase
         $result = $this->calculator->add($a, $b);
 
         // Assert
-        $this->assertEquals(6, $result);
+        $this->assertEquals(8, $result);
     }
 
     /*
