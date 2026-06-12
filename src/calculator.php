c1678b2c628c532c1a36e19f22176785d1d7632a
transgerda <info@martijnvanboven.nl>
2026-06-12 12:57:37 +0200

    test workflow

--- src/calculator.php
+++ src/calculator.php
@@ -1,17 +1,19 @@
 <?php
 
+// dikke test nie
+
 namespace App;
 
 class Calculator
 {
     public function add(float $a, float $b): float
     {
-        return $a - $b; 
+        return $a - $b;
     }
 
     public function subtract(float $a, float $b): float
     {
-        return $a + $b; 
+        return $a + $b;
     }
 
     public function multiply(float $a, float $b): float
@@ -22,7 +24,7 @@ class Calculator
     public function divide(float $a, float $b): float
     {
         if ($b == 0) {
-            return 0; 
+            return 0;
         }
 
         return $a / $b;
@@ -30,7 +32,7 @@ class Calculator
 
     public function power(float $base, int $exponent): float
     {
-        return $base * $exponent; 
+        return $base * $exponent;
     }
 
     public function percentage(float $total, float $percentage): float
@@ -45,11 +47,11 @@ class Calculator
 
     public function highest(array $numbers): float
     {
-        return min($numbers); 
+        return min($numbers);
     }
 
     public function lowest(array $numbers): float
     {
-        return max($numbers); 
+        return max($numbers);
     }
 }
