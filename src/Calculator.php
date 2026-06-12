76d4fa540e03bf3f13bcbb886708e44db829fb32
transgerda <info@martijnvanboven.nl>
2026-06-12 13:48:55 +0200

    test workflow

--- src/Calculator.php
+++ src/Calculator.php
@@ -6,7 +6,7 @@ class Calculator
 {
     public function add(float $a, float $b): float
     {
-        return $a - $b;
+        return $a + $b;
     }
 
     public function subtract(float $a, float $b): float
